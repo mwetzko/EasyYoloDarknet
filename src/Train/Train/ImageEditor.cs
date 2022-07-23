608642f38c637f60009c942abad279de486a38b3
mwetzko <info@mwetzko.com>
2022-07-23 11:51:26 +0200

    selection frame fit to image bounding area, use rect instead of point and size, use int instead of float for pixel calc

--- src/Train/Train/ImageEditor.cs
+++ src/Train/Train/ImageEditor.cs
@@ -9,10 +9,10 @@ namespace Train
         Image mImage;
         int mScale = 100;
         int mScaleMultiplier = 1;
-        PointF mMouseLocation;
-        PointF mImagePos;
-        SizeF mImageSize;
+        Point mMouseLocation;
+        Rectangle mImageRect;
         bool mFromCenter;
+        Rectangle mSelection;
 
         public ImageEditor()
         {
@@ -35,13 +35,21 @@ namespace Train
             {
                 mImage = value;
 
-                if (mImage != null)
+                if (this.HasImage)
                 {
                     CalcImageSize();
                     CenterImage();
 
                     bufferPanel.Invalidate();
                 }
+            }
+        }
+
+        public bool HasImage
+        {
+            get
+            {
+                return mImage != null;
             }
         }
 
@@ -53,63 +61,108 @@ namespace Train
         void CenterImage()
         {
             mFromCenter = true;
-            mImagePos.X = (bufferPanel.Width / 2f) - (mImageSize.Width / 2f);
-            mImagePos.Y = (bufferPanel.Height / 2f) - (mImageSize.Height / 2f);
+
+            mImageRect.X = (int)((bufferPanel.Width / 2f) - (mImageRect.Width / 2f));
+            mImageRect.Y = (int)((bufferPanel.Height / 2f) - (mImageRect.Height / 2f));
         }
 
         void CalcImageSize()
         {
             var s = mScale / 100f;
 
-            mImageSize.Width = this.Image.Width * s;
-            mImageSize.Height = this.Image.Height * s;
+            mImageRect.Width = (int)(this.Image.Width * s);
+            mImageRect.Height = (int)(this.Image.Height * s);
         }
 
-        void CalcRelativeImagePos(SizeF formerImageSize)
+        void CalcRelativeImagePos(Size formerImageSize)
         {
-            if (mMouseLocation.X < mImagePos.X || mMouseLocation.X > (mImagePos.X + formerImageSize.Width))
+            if (mImageRect.Contains(mMouseLocation))
             {
-                mImagePos.X = mImagePos.X + (formerImageSize.Width / 2) - (mImageSize.Width / 2);
+                mImageRect.X = mMouseLocation.X - (int)(((mMouseLocation.X - mImageRect.X) / (float)formerImageSize.Width) * mImageRect.Width);
+                mImageRect.Y = mMouseLocation.Y - (int)(((mMouseLocation.Y - mImageRect.Y) / (float)formerImageSize.Height) * mImageRect.Height);
             }
             else
             {
-                mImagePos.X = mMouseLocation.X - (((mMouseLocation.X - mImagePos.X) / formerImageSize.Width) * mImageSize.Width);
-            }
-
-            if (mMouseLocation.Y < mImagePos.Y || mMouseLocation.Y > (mImagePos.Y + formerImageSize.Height))
-            {
-                mImagePos.Y = mImagePos.Y + (formerImageSize.Height / 2) - (mImageSize.Height / 2);
-            }
-            else
-            {
-                mImagePos.Y = mMouseLocation.Y - (((mMouseLocation.Y - mImagePos.Y) / formerImageSize.Height) * mImageSize.Height);
+                mImageRect.X = mImageRect.X + (formerImageSize.Width / 2) - (mImageRect.Width / 2);
+                mImageRect.Y = mImageRect.Y + (formerImageSize.Height / 2) - (mImageRect.Height / 2);
             }
         }
 
         void bufferPanel_Paint(object sender, PaintEventArgs e)
         {
-            if (this.Image != null)
+            if (this.HasImage)
             {
-                e.Graphics.DrawImage(this.Image, mImagePos.X, mImagePos.Y, mImageSize.Width, mImageSize.Height);
+                e.Graphics.DrawImage(this.Image, mImageRect);
+            }
+
+            if (mSelection.Width != 0 && mSelection.Height != 0)
+            {
+                Rectangle rect = new Rectangle();
+
+                if (mSelection.Width > 0)
+                {
+                    rect.X = mSelection.X;
+                    rect.Width = mSelection.Width;
+                }
+                else
+                {
+                    rect.X = mSelection.X + mSelection.Width;
+                    rect.Width = -mSelection.Width;
+                }
+
+                if (mSelection.Height > 0)
+                {
+                    rect.Y = mSelection.Y;
+                    rect.Height = mSelection.Height;
+                }
+                else
+                {
+                    rect.Y = mSelection.Y + mSelection.Height;
+                    rect.Height = -mSelection.Height;
+                }
+
+                rect = Rectangle.Intersect(mImageRect, rect);
+
+                ControlPaint.DrawFocusRectangle(e.Graphics, rect);
             }
         }
 
         void bufferPanel_MouseDown(object sender, MouseEventArgs e)
         {
             mMouseLocation = e.Location;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                mSelection = new Rectangle(mMouseLocation, new Size(0, 0));
+            }
         }
 
         void bufferPanel_MouseUp(object sender, MouseEventArgs e)
         {
+            if (e.Button == MouseButtons.Left)
+            {
+                mSelection.Width = 0;
+                mSelection.Height = 0;
+
+                bufferPanel.Invalidate();
+            }
+
             mMouseLocation = e.Location;
         }
 
         void bufferPanel_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Right && this.Image != null)
+            if (e.Button == MouseButtons.Left)
             {
-                mImagePos.X += (e.X - mMouseLocation.X);
-                mImagePos.Y += (e.Y - mMouseLocation.Y);
+                mSelection.Width = e.X - mSelection.Location.X;
+                mSelection.Height = e.Y - mSelection.Location.Y;
+
+                bufferPanel.Invalidate();
+            }
+            else if (e.Button == MouseButtons.Right && this.HasImage)
+            {
+                mImageRect.X += (e.X - mMouseLocation.X);
+                mImageRect.Y += (e.Y - mMouseLocation.Y);
 
                 bufferPanel.Invalidate();
             }
@@ -119,60 +172,66 @@ namespace Train
 
         void bufferPanel_MouseWheel(object sender, MouseEventArgs e)
         {
-            mFromCenter = false;
-
-            if (e.Delta < 0)
+            if (this.HasImage)
             {
-                if (mScaleMultiplier > 1)
+                if (e.Button == MouseButtons.None)
                 {
-                    mScaleMultiplier -= 1;
+                    mFromCenter = false;
 
-                    mScale -= (20 * mScaleMultiplier);
+                    if (e.Delta < 0)
+                    {
+                        if (mScaleMultiplier > 1)
+                        {
+                            mScaleMultiplier -= 1;
+
+                            mScale -= (20 * mScaleMultiplier);
+                        }
+                        else if (mScale > 20)
+                        {
+                            mScale -= 20;
+                        }
+                    }
+                    else
+                    {
+                        if (mScale < 100)
+                        {
+                            mScale += 20;
+                        }
+                        else if (mScaleMultiplier < 10)
+                        {
+                            mScale += (20 * mScaleMultiplier);
+
+                            mScaleMultiplier += 1;
+                        }
+                    }
+
+                    UpdateScaleText();
+
+                    Size sz = mImageRect.Size;
+
+                    CalcImageSize();
+                    CalcRelativeImagePos(sz);
+
+                    bufferPanel.Invalidate();
                 }
-                else if (mScale > 20)
-                {
-                    mScale -= 20;
-                }
-            }
-            else
-            {
-                if (mScale < 100)
-                {
-                    mScale += 20;
-                }
-                else if (mScaleMultiplier < 10)
-                {
-                    mScale += (20 * mScaleMultiplier);
-
-                    mScaleMultiplier += 1;
-                }
-            }
-
-            UpdateScaleText();
-
-            if (this.Image != null)
-            {
-                SizeF sz = mImageSize;
-
-                CalcImageSize();
-                CalcRelativeImagePos(sz);
-
-                bufferPanel.Invalidate();
             }
         }
 
         void bufferPanel_Resize(object sender, EventArgs e)
         {
-            if (mFromCenter)
+            if (this.HasImage)
             {
-                CenterImage();
-            }
-            else
-            {
-                CalcRelativeImagePos(mImageSize);
-            }
+                if (mFromCenter)
+                {
+                    CenterImage();
+                }
+                else
+                {
+                    CalcRelativeImagePos(mImageRect.Size);
+                }
 
-            bufferPanel.Invalidate();
+                bufferPanel.Invalidate();
+            }
         }
     }
 }
